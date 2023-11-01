30dec2ce47b19a37b647d5af9c0596f6f6b9e5b3
zhanghengxin <ezreal.zhang@icewhale.org>
2023-11-01 15:29:38 +0800

    :fire: remove icon.

--- src/views/AppLauncherCheck.vue
+++ src/views/AppLauncherCheck.vue
@@ -8,23 +8,21 @@
   -->
 <template>
 	<div v-if="isCheckFailed"
-		 class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center is-fullheight">
-		<b-image :key="appDetailData.icon" :src="appDetailData.icon"
-				 :src-fallback="require('@/assets/img/app/default.svg')"
-				 class="is-64x64 icon-shadow" webp-fallback=".jpg"></b-image>
+		class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center is-fullheight">
+		<b-image :key="appDetailData.icon" :src="appDetailData.icon" :src-fallback="require('@/assets/img/app/default.svg')"
+			class="is-64x64 icon-shadow" webp-fallback=".jpg"></b-image>
 		<h2 class="has-text-emphasis-01 has-text-white mt-2">{{ appDetailData.name }}</h2>
 		<h1 v-if="status === 'pending'" class="has-text-sub-03 has-text-white mt-6">{{ $t('Preparing for launch') }}
 		</h1>
 		<h1 v-else class="has-text-sub-03 has-text-white mt-6">{{ $t('APP may not be available') }}</h1>
 		<b-image v-if="status === 'pending'" :src="require('@/assets/img/loading/waiting.svg')" alt="pending"
-				 class="is-48x48 mt-6"/>
+			class="is-48x48 mt-6" />
 		<span v-else class="has-text-full-03 has-text-grey-600 mt-6">{{
-				$t('Please')
-			}}
-      <a @click="openThirdApp(appDetailData);">{{ $t('Click here') }}
-      </a> {{ $t('to open the app. If it does not work, please restart or try again later.') }}
-    </span>
-		<img :src="require('@/assets/img/logo/logo.svg')" alt="" class="is-absolute position"/>
+			$t('Please')
+		}}
+			<a @click="openThirdApp(appDetailData);">{{ $t('Click here') }}
+			</a> {{ $t('to open the app. If it does not work, please restart or try again later.') }}
+		</span>
 	</div>
 </template>
 
@@ -108,10 +106,5 @@ export default {
 .is-fullheight {
 	background: hsla(208, 20%, 12%, 1);
 	height: 100vh;
-}
-
-.position {
-	left: 2rem;
-	bottom: 1.25rem;
 }
 </style>
