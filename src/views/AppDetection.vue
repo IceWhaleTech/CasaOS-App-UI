e725b4665c01fdd3ca52839a79f6638a06e41e2a
zhanghengxin <ezreal.zhang@icewhale.org>
2024-03-13 15:53:19 +0800

    format

--- src/views/AppDetection.vue
+++ src/views/AppDetection.vue
@@ -3,50 +3,77 @@
 	<div v-else class="hero is-fullheight has-background-black">
 		<div class="hero-body is-flex is-flex-direction-column">
 			<div
-				class="container has-text-white is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+				class="container has-text-white is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+			>
 				<div class="is-flex is-align-items-center mb-3">
-					<b-image :key="activeAppData.icon + 1" :src="activeAppData.icon"
-						:src-fallback="require('@/assets/img/app/default.svg')" class="is-64x64 icon-shadow"
-						webp-fallback=".jpg"></b-image>
+					<b-image
+						:key="activeAppData.icon + 1"
+						:src="activeAppData.icon"
+						:src-fallback="require('@/assets/img/app/default.svg')"
+						class="is-64x64 icon-shadow"
+						webp-fallback=".jpg"
+					></b-image>
 
-					<svg v-show="!isStoppingApp" width="24" height="24" viewBox="0 0 24 24" fill="none"
-						xmlns="http://www.w3.org/2000/svg" class="mx-4">
+					<svg
+						v-show="!isStoppingApp"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						class="mx-4"
+					>
 						<path
 							d="M0 12C0.0312502 14.1875 0.562495 16.1875 1.59375 18C2.65626 19.8125 4.12499 21.2812 6 22.4063C7.90626 23.4688 9.90624 24 12 24C14.0938 24 16.0937 23.4688 18 22.4063C19.875 21.2812 21.3437 19.8125 22.4063 18C23.4375 16.1875 23.9688 14.1875 24 12C23.9688 9.81249 23.4375 7.81251 22.4063 6C21.3437 4.18749 19.875 2.71876 18 1.59375C16.0937 0.531244 14.0938 0 12 0C9.90624 0 7.90626 0.531244 6 1.59375C4.12499 2.71876 2.65626 4.18749 1.59375 6C0.562495 7.81251 0.0312502 9.81249 0 12ZM11.2969 17.6719C10.7656 18.1094 10.2344 18.1094 9.70313 17.6719C9.26562 17.1406 9.26562 16.6094 9.70313 16.0781L13.7813 12L9.70313 7.92188C9.26562 7.39062 9.26562 6.85938 9.70313 6.32813C10.2344 5.89062 10.7656 5.89062 11.2969 6.32813L16.1719 11.2031C16.6094 11.7344 16.6094 12.2656 16.1719 12.7969L11.2969 17.6719Z"
-							fill="#303233" />
+							fill="#303233"
+						/>
 					</svg>
 
-					<b-image v-show="!isStoppingApp" :key="appDetailData.name + 2" :src="appDetailData.icon"
-						:src-fallback="require('@/assets/img/app/default.svg')" class="is-64x64 icon-shadow"
-						webp-fallback=".jpg"></b-image>
+					<b-image
+						v-show="!isStoppingApp"
+						:key="appDetailData.name + 2"
+						:src="appDetailData.icon"
+						:src-fallback="require('@/assets/img/app/default.svg')"
+						class="is-64x64 icon-shadow"
+						webp-fallback=".jpg"
+					></b-image>
 				</div>
 				<div class="has-text-full-02 mb-5">
 					{{ ice_i18n(appDetailData.title) }}
 				</div>
 				<div class="has-text-full-02 mt-4 mb-2">
 					{{
-					isStoppingApp
-					? $t(contentText, { name: "" }) + ice_i18n(activeAppData.title)
-					: $t(contentText, {
-					name: "",
-					})
+						isStoppingApp
+							? $t(contentText, { name: '' }) + ice_i18n(activeAppData.title)
+							: $t(contentText, {
+									name: ''
+							  })
 					}}
 				</div>
 				<div v-show="remakeText" class="has-text-full-04 op40 mb-5">
 					{{ $t(remakeText) }}
 				</div>
 				<div v-show="remakeText && isFailed" class="has-text-info cursor-pointer" @click="closePage">
-					{{ $t("close") }}
+					{{ $t('close') }}
 				</div>
-				<b-image v-if="isPending" :src="require('@/assets/img/loading/waiting.svg')" alt="pending"
-					class="is-48x48 mt-6" />
+				<b-image
+					v-if="isPending"
+					:src="require('@/assets/img/loading/waiting.svg')"
+					alt="pending"
+					class="is-48x48 mt-6"
+				/>
 				<div v-else-if="!isFailed" class="mt-4 is-flex is-justify-content-space-between">
 					<b-button rounded class="has-text-white close-app-button mr-1" @click="closePage">
-						{{ $t("Continue {name}", { name: "" }) + ice_i18n(activeAppData.title) }}
+						{{ $t('Continue {name}', { name: '' }) + ice_i18n(activeAppData.title) }}
 					</b-button>
-					<b-button type="is-primary" :loading="isLoading" rounded class="ml-1"
-						@click="switchToApp(appDetailData.name)">
-						{{ $t("Switch to {name}", { name: "" }) + ice_i18n(appDetailData.title) }}
+					<b-button
+						type="is-primary"
+						:loading="isLoading"
+						rounded
+						class="ml-1"
+						@click="switchToApp(appDetailData.name)"
+					>
+						{{ $t('Switch to {name}', { name: '' }) + ice_i18n(appDetailData.title) }}
 					</b-button>
 				</div>
 			</div>
@@ -55,148 +82,144 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted, reactive, nextTick } from "vue";
-import ProgressSpinner from 'primevue/progressspinner';
-import { useRoute } from "vue-router/composables";
+import { ref, computed, onMounted, reactive, nextTick } from 'vue'
+import ProgressSpinner from 'primevue/progressspinner'
+import { useRoute } from 'vue-router/composables'
 // import { usei18n } from "@/composables/usei18n";
-import { useOpenApp } from "@/composables/useOpenApp";
-import { iceGpu } from "@/service/index.js";
-import { GPUApplicationStatusEnum } from "@icewhale/zimaos-openapi";
-import { ice_i18n } from "@/mixins/base/common-i18n";
-import { openDB } from "idb";
+import { useOpenApp } from '@/composables/useOpenApp'
+import { iceGpu } from '@/service/index.js'
+import { GPUApplicationStatusEnum } from '@icewhale/zimaos-openapi'
+import { ice_i18n } from '@/mixins/base/common-i18n'
+import { openDB } from 'idb'
 
-const openApp = useOpenApp();
+const openApp = useOpenApp()
 // const { i18n: t } = usei18n();
-const route = useRoute();
-const activeAppData = ref({ icon: "", name: "", title: {} });
-const appDetailData = ref({ icon: "", name: "", title: {} });
-const currentAppName = route.query.name;
-const isStoppingApp = ref(false);
-const isStarttingApp = ref(false);
-const contentText = ref("Insufficient performance to run simultaneously");
-const remakeText = ref("Detected performance conflict");
-const isFailed = ref(false);
-const isLoading = ref(false);
-const isLoadingPage = ref(true);
-const isPending = computed(() => isStoppingApp.value || isStarttingApp.value);
+const route = useRoute()
+const activeAppData = ref({ icon: '', name: '', title: {} })
+const appDetailData = ref({ icon: '', name: '', title: {} })
+const currentAppName = route.query.name
+const isStoppingApp = ref(false)
+const isStarttingApp = ref(false)
+const contentText = ref('Insufficient performance to run simultaneously')
+const remakeText = ref('Detected performance conflict')
+const isFailed = ref(false)
+const isLoading = ref(false)
+const isLoadingPage = ref(true)
+const isPending = computed(() => isStoppingApp.value || isStarttingApp.value)
 
-let db;
+let db
 
-function closePage() {
+function closePage () {
 	// 关闭当前页面
-	window.close();
+	window.close()
 }
 
-async function stopApp() {
-	isStoppingApp.value = true;
-	contentText.value = "Closing {name}";
-	remakeText.value = "";
+async function stopApp () {
+	isStoppingApp.value = true
+	contentText.value = 'Closing {name}'
+	remakeText.value = ''
 
 	try {
 		await iceGpu
 			.setGPUApplicationsStatus(activeAppData.value.name, {
-				status: GPUApplicationStatusEnum.Stop,
+				status: GPUApplicationStatusEnum.Stop
 			})
-			.then((res) => {
+			.then(res => {
 				if (res.status === 200) {
-					isStoppingApp.value = false;
+					isStoppingApp.value = false
 				}
-			});
-		return true;
+			})
+		return true
 	} catch (error) {
-		console.error("close error", error);
-		isStoppingApp.value = false;
-		contentText.value = "Cannot close {name}";
-		remakeText.value = "Please right-click on the dashboard and try closing it again";
-		isFailed.value = true;
-		return false;
+		console.error('close error', error)
+		isStoppingApp.value = false
+		contentText.value = 'Cannot close {name}'
+		remakeText.value = 'Please right-click on the dashboard and try closing it again'
+		isFailed.value = true
+		return false
 	}
 }
 
-async function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+async function sleep (ms) {
+	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function startApp() {
-	isStarttingApp.value = true;
-	contentText.value = "Launching {name}";
+function startApp () {
+	isStarttingApp.value = true
+	contentText.value = 'Launching {name}'
 	iceGpu
 		.setGPUApplicationsStatus(appDetailData.value.name, {
-			status: GPUApplicationStatusEnum.Start,
+			status: GPUApplicationStatusEnum.Start
 		})
-		.then(async (res) => {
+		.then(async res => {
 			if (res.status === 200) {
-				const targetApp = await db.get('app', appDetailData.value.name);
-				let runningAppLength = 2, runningAppName, times = 0;
+				const targetApp = await db.get('app', appDetailData.value.name)
+				let runningAppLength = 2,
+					runningAppName,
+					times = 0
 				while ((runningAppLength === 1 && runningAppName === appDetailData.value.name) || times > 30) {
-					[runningAppLength, runningAppName] = await getRunningGPUApps();
-					await sleep(2000);
-					times++;
+					;[runningAppLength, runningAppName] = await getRunningGPUApps()
+					await sleep(2000)
+					times++
 				}
-				isLoading.value = false;
-				isStarttingApp.value = false;
+				isLoading.value = false
+				isStarttingApp.value = false
 
-				runningAppLength === 1 && runningAppName === appDetailData.value.name && openApp(targetApp);
+				runningAppLength === 1 && runningAppName === appDetailData.value.name && openApp(targetApp)
 			}
 		})
-		.catch((e) => {
-			console.error(e);
-			isLoading.value = false;
-			isStarttingApp.value = false;
-			contentText.value = "Cannot launch {name}";
-			remakeText.value = "Please right-click on the dashboard and try switching it again";
-			isFailed.value = true;
-		});
+		.catch(e => {
+			console.error(e)
+			isLoading.value = false
+			isStarttingApp.value = false
+			contentText.value = 'Cannot launch {name}'
+			remakeText.value = 'Please right-click on the dashboard and try switching it again'
+			isFailed.value = true
+		})
 }
 
-async function switchToApp(appName) {
-	isLoading.value = true;
+async function switchToApp (appName) {
+	isLoading.value = true
 
 	// first close the current app
-	const result = await stopApp();
+	const result = await stopApp()
 	if (!result) {
-		isLoading.value = false;
-		return;
+		isLoading.value = false
+		return
 	}
-	startApp();
+	startApp()
 }
 
-async function getRunningGPUApps(){
-	const res = await iceGpu.getGPUApplications();
-	const appList = res.data.data || [];
-	const runningApps = appList.filter((item) => item.status === GPUApplicationStatusEnum.Running);
-	return [runningApps.length, runningApps?.[0]?.name];
+async function getRunningGPUApps () {
+	const res = await iceGpu.getGPUApplications()
+	const appList = res.data.data || []
+	const runningApps = appList.filter(item => item.status === GPUApplicationStatusEnum.Running)
+	return [runningApps.length, runningApps?.[0]?.name]
 }
 
 onMounted(async () => {
-	db = await openDB('casaos', 1);
+	db = await openDB('casaos', 1)
 
-	const [runningAppLength, runningAppName] = await getRunningGPUApps();
+	const [runningAppLength, runningAppName] = await getRunningGPUApps()
 
-	if ((runningAppLength === 1 && runningAppName === currentAppName) || runningAppLength === 0) { 
-		const targetApp = await db.get('app', currentAppName); 
-		openApp(targetApp); 
-		return; 
+	if ((runningAppLength === 1 && runningAppName === currentAppName) || runningAppLength === 0) {
+		const targetApp = await db.get('app', currentAppName)
+		openApp(targetApp)
+		return
 	}
 
-	isLoadingPage.value = false; 
-	// TODO: data model is not unified. 
-	const app = appList.find(
-		(item) => 
-			item.status === GPUApplicationStatusEnum.Running && item.name !== currentAppName 
-	); 
-	appDetailData.value = appList.find( 
-		(item) => 
-			item.name === currentAppName 
-	); 
-	if (app) { 
-		activeAppData.value = app; 
-	} else { 
-		nextTick(() => { 
-			startApp();
-		}); 
+	isLoadingPage.value = false
+	// TODO: data model is not unified.
+	const app = appList.find(item => item.status === GPUApplicationStatusEnum.Running && item.name !== currentAppName)
+	appDetailData.value = appList.find(item => item.name === currentAppName)
+	if (app) {
+		activeAppData.value = app
+	} else {
+		nextTick(() => {
+			startApp()
+		})
 	}
-});
+})
 </script>
 
 <style scoped>
