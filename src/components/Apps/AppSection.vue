eca0b6ab3abc3a030a18fb53c93e16dc9681d728
zhanghengxin <ezreal.zhang@icewhale.org>
2024-05-14 10:19:23 +0800

    fix: Handle error when fetching GPU list in AppSection.vue

--- src/components/Apps/AppSection.vue
+++ src/components/Apps/AppSection.vue
@@ -233,11 +233,16 @@ export default {
 			try {
 				const hasGpu = await iceGpu.getGPUList(10*1024*1024*1024, true).then((res) => {
 					return res.data.data.length > 0;
+				}).catch(() => {
+					return false;
 				});
 				if (this.gpuAppList.length === 0) {
 					this.gpuAppList = await iceGpu
 						.getGPUApplications()
-						.then((res) => res.data.data || []);
+						.then((res) => res.data.data || [])
+						.catch(() => {
+							return [];
+						});
 				}
 
 				const orgAppList = await this.$openAPI.appGrid
