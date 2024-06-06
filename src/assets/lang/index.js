490d0d66d88d25d0ad58cc5d2ef8e19bc8eef2a2
John Guan <Guan.Ningchuan@gmail.com>
2024-06-06 16:55:00 +0800

    add ja_JP (#88)

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,18 +1,10 @@
-/*
- * @Author: Jerryk jerry@icewhale.org
- * @Date: 2023-01-16 14:25:05
- * @LastEditors: Jerryk jerry@icewhale.org
- * @LastEditTime: 2023-01-16 16:11:55
- * @FilePath: /CasaOS-UI/src/assets/lang/index.js
- * @Description: 
- * 
- * Copyright (c) 2023 by IceWhale, All Rights Reserved. 
- */
 import zh_cn from './zh_CN.json' // Chinese: 简体中文
 import en_us from './en_US.json' // English: English
+import ja_jp from './ja_JP.json' // Japanese: 日本語
 
 
 export default {
 	zh_cn,
 	en_us,
+	ja_jp
 }
