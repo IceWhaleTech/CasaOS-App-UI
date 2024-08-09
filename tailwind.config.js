904b17da8704bfd06ee4d3be5668a57aa3afab8b
John Guan <Guan.Ningchuan@gmail.com>
2024-08-09 17:55:08 +0800

    BrittiSans  as default font (#94)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	content: ["./src/**/*.{vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['BrittiSans', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
