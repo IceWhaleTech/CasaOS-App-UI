884e5b77af26144e0550ab324e726240b59b5ed8
John Guan <Guan.Ningchuan@gmail.com>
2024-08-23 14:36:41 +0800

    add migration (#96)

---
+++ src/components/AppSetting/AppMigrationBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-tooltip type="is-black" position="is-right" :label="$t('Migrate user data to another space.')">
+    <b-button
+      rounded
+      type="is-info is-light"
+      size="is-small"
+      @click="
+        $messageBus('mircoapp_communicate', {
+          action: 'open',
+          peerType: 'settings',
+          name: 'icewhale_settings',
+          routerPath: '/migration',
+        })
+      "
+    >
+      {{ $t("Migration") }}
+    </b-button>
+  </b-tooltip>
+</template>
+
+<script>
+export default {
+  name: "AppMigrationBtn",
+};
+</script>
