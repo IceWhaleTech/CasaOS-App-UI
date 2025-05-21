255c6717720f1742c89ff94b50b69c5aae8905ab
老竭力 <jerrykuku@qq.com>
2025-05-21 19:02:06 +0800

    Update language files and add new translations for Croatian and Turkish

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -5,7 +5,14 @@ import en_gb from './en_GB.json' // English: English (UK)
 import it_it from './it_IT.json' // Italian: Italiano
 import ko_kr from './ko_KR.json' // Korean: 한국어
 import nb_no from './nb_NO.json' // Norwegian: Norskh
-
+import ru_ru from './ru_RU.json' // Russian: Русский
+import sv_se from './sv_SE.json' // Swedish: Svenska
+import tr_tr from './tr_TR.json' // Turkish: Türkçe
+import de_de from './de_DE.json' // German: Deutsch
+import fr_fr from './fr_FR.json' // French: Français
+import el_gr from './el_GR.json' // Greek: Ελληνικά
+import pt_pt from './pt_PT.json' // Portuguese: Português
+import hr_hr from './hr_HR.json' // Croatian: Hrvatski
 
 export default {
 	zh_cn,
@@ -14,5 +21,13 @@ export default {
 	en_gb,
 	it_it,
 	ko_kr,
-	nb_no
+	nb_no,
+	ru_ru,
+	sv_se,
+	tr_tr,
+	de_de,
+	fr_fr,
+	el_gr,
+	pt_pt,
+	hr_hr
 }
