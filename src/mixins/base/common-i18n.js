dd9202fe398a502312219a9f0e3a257b2439d0c5
老竭力 <jerrykuku@qq.com>
2025-05-22 17:04:10 +0800

    update i18n for app store (#132)

--- src/mixins/base/common-i18n.js
+++ src/mixins/base/common-i18n.js
@@ -1,15 +1,6 @@
-/*
- * @LastEditors: zhanghengxin ezreal.zhang@icewhale.org
- * @LastEditTime: 2023/4/10 下午5:49
- * @FilePath: /CasaOS-UI/src/mixins/base/common-i18n.js
- * @Description:
- *
- * Copyright (c) 2023 by IceWhale, All Rights Reserved.
-
- */
 
 const i18n = function (data, fallback = 'en_us') {
-	let lang = localStorage.getItem('lang')
+	let lang = localStorage.getItem('lang').toLocaleLowerCase();
 	return data?.['custom'] || data?.[lang] || data?.[fallback] || data?.['en_US'];
 }
 
