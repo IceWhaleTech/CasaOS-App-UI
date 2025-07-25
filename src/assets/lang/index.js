cb0a5754c6e09ef7d1abfb67a8cb4ad8d636b12f
老竭力 <jerrykuku@qq.com>
2025-07-25 13:15:53 +0800

    update translation

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,10 +1,11 @@
 import zh_cn from './zh_CN.json' // Chinese: 简体中文
+import zh_tw from './zh_TW.json' // Chinese: 繁體中文
 import en_us from './en_US.json' // English: English (US)
-import ja_jp from './ja_JP.json' // Japanese: 日本語
 import en_gb from './en_GB.json' // English: English (UK)
-import it_it from './it_IT.json' // Italian: Italiano
+import ja_jp from './ja_JP.json' // Japanese: 日本語
 import ko_kr from './ko_KR.json' // Korean: 한국어
-import nb_no from './nb_NO.json' // Norwegian: Norskh
+import it_it from './it_IT.json' // Italian: Italiano
+import nb_no from './nb_NO.json' // Norwegian: Norsk
 import ru_ru from './ru_RU.json' // Russian: Русский
 import sv_se from './sv_SE.json' // Swedish: Svenska
 import tr_tr from './tr_TR.json' // Turkish: Türkçe
@@ -12,15 +13,28 @@ import de_de from './de_DE.json' // German: Deutsch
 import fr_fr from './fr_FR.json' // French: Français
 import el_gr from './el_GR.json' // Greek: Ελληνικά
 import pt_pt from './pt_PT.json' // Portuguese: Português
+import pt_br from './pt_BR.json' // Portuguese: Português (Brasil)
 import hr_hr from './hr_HR.json' // Croatian: Hrvatski
+import sk_sk from './sk_SK.json' // Slovak: Slovenčina
+import ro_ro from './ro_RO.json' // Romanian: Română
+import pl_pl from './pl_PL.json' // Polish: Polski
+import nl_nl from './nl_NL.json' // Dutch: Nederlands
+import ml_in from './ml_IN.json' // Malayalam: മലയാളം
+import hu_hu from './hu_HU.json' // Hungarian: Magyar
+import ga_ie from './ga_IE.json' // Irish: Gaeilge
+import es_es from './es_ES.json' // Spanish: Español
+import da_dk from './da_DK.json' // Danish: Dansk
+import cs_cz from './cs_CZ.json' // Czech: Čeština
+import ca_es from './ca_ES.json' // Catalan: Català
 
 export default {
 	zh_cn,
+	zh_tw,
 	en_us,
-	ja_jp,
 	en_gb,
-	it_it,
+	ja_jp,
 	ko_kr,
+	it_it,
 	nb_no,
 	ru_ru,
 	sv_se,
@@ -29,5 +43,17 @@ export default {
 	fr_fr,
 	el_gr,
 	pt_pt,
-	hr_hr
+	pt_br,
+	hr_hr,
+	sk_sk,
+	ro_ro,
+	pl_pl,
+	nl_nl,
+	ml_in,
+	hu_hu,
+	ga_ie,
+	es_es,
+	da_dk,
+	cs_cz,
+	ca_es
 }
